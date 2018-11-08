bd62a75d67228040bda2d74f0754bbf5f770824d
Aldo <>
2018-11-08 21:31:34 +0000

    Dont add the char codes

--- stream.js
+++ stream.js
@@ -6,22 +6,18 @@ function getSessionPort(s)
     }
     var bufferLength = s.buffer.length;
     var version = s.buffer.charCodeAt(1);
-    s.error("Buffer " + s.buffer);
 
 
     var sessionPort = 0;
     if(version === 1)
     {
-        for(var i = 0; i < bufferLength; ++i){
-            s.error("Byte[" + i + "]" + s.buffer.charCodeAt(i));
-        }
-        sessionPort = String.fromCharCode(s.buffer.charCodeAt(4) + 
-        s.buffer.charCodeAt(5) + 
-        s.buffer.charCodeAt(6) + 
-        s.buffer.charCodeAt(7) + 
-        s.buffer.charCodeAt(8) + 
-        s.buffer.charCodeAt(9) + 
-        s.buffer.charCodeAt(10) +
+        sessionPort = String.fromCharCode(s.buffer.charCodeAt(4),
+        s.buffer.charCodeAt(5),
+        s.buffer.charCodeAt(6), 
+        s.buffer.charCodeAt(7), 
+        s.buffer.charCodeAt(8), 
+        s.buffer.charCodeAt(9), 
+        s.buffer.charCodeAt(10),
         s.buffer.charCodeAt(11));
     }
     else {
@@ -30,5 +26,5 @@ function getSessionPort(s)
 
     s.error("Parsed session port" + sessionPort);
 
-    return 531285;
+    return sessionPort;
 }   
