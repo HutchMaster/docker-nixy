1ad23574e0e92fa54f1f4b3aed698d8f5a0ce514
Aldo <>
2018-11-08 21:11:25 +0000

    Log more about the buffers bytes

--- stream.js
+++ stream.js
@@ -6,10 +6,15 @@ function getSessionPort(s)
     }
     var bufferLength = s.buffer.length;
     var version = s.buffer.charCodeAt(1);
+    s.error(s.buffer);
+
 
     var sessionPort = 0;
     if(version === 1)
     {
+        for(var i = 0; i < bufferLength; ++i){
+            s.error("Byte[" + i + "]" + s.buffer.charCodeAt(i));
+        }
         sessionPort = ((s.buffer.charCodeAt(19)) |
         s.buffer.charCodeAt(20) << 8);
     }
@@ -20,4 +25,4 @@ function getSessionPort(s)
     s.error(sessionPort.toString());
 
     return 531285;
-}
+}   
