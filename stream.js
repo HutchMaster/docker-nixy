133b7219e9fc7c9a455fd9b25dd60b0b93a2514e
Geoff Pate <geoff.pate@hutchgames.com>
2021-05-25 07:05:51 +0100

    Move to NGINX 1.19.10

--- stream.js
+++ stream.js
@@ -1,30 +1,35 @@
-function getSessionPort(s)
-{
-    if(s.fromUpstream)
-    {
-        return null;
-    }
-    var bufferLength = s.buffer.length;
-    var version = s.buffer.charCodeAt(1);
+var server = null;
 
+function getUpstream(s) {
+    s.log("s.remoteAddress: " + s.remoteAddress);
+    s.log("returning upstream: " + server);
+    return server;
+}
 
-    var rawIp = "";
-    var rawPort = "";
+function readLastOctet(s) {
+    s.log("s.remoteAddress: " + s.remoteAddress);
 
-    rawIp = String.fromCharCode(
-    s.buffer.charCodeAt(4),
-    s.buffer.charCodeAt(5),
-    s.buffer.charCodeAt(6));
+    s.log("server: " + server);
 
-    rawPort = String.fromCharCode(
-    s.buffer.charCodeAt(7), 
-    s.buffer.charCodeAt(8), 
-    s.buffer.charCodeAt(9), 
-    s.buffer.charCodeAt(10),
-    s.buffer.charCodeAt(11));
-    
-
-    var rawRead = parseInt(rawIp).toString() + parseInt(rawPort).toString();
-    
-    return rawRead;
+    var req = "";
+    s.on("upload", function(data, flags) {
+        req += data;
+        if (req.length >= 11) {
+            s.log("req: " + req);
+            var version = req.charCodeAt(1);
+            s.log("version: " + version);
+                    
+            var lastOctet = String.fromCharCode(
+                req.charCodeAt(4),
+                req.charCodeAt(5),
+                req.charCodeAt(6));
+            s.log("lastOctet: " + parseInt(lastOctet).toString());                        
+            
+            server = "10.32.0." + parseInt(lastOctet).toString() + ":10500";
+            s.off("upload");
+            return s.done();
+        }
+    });
 }   
+
+export default { readLastOctet, getUpstream };
